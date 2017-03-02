cb34e139036017eb64e2a987fe904e617c182d0e
sswukang <sswukang@gmail.com>
2017-03-02 15:46:31 +0800

    新增StickyAdapter示例

--- app/src/main/java/cn/sswukang/example/util/BindingUtils.java
+++ app/src/main/java/cn/sswukang/example/util/BindingUtils.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.ColorInt;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 
@@ -19,46 +20,75 @@ import cn.sswukang.library.lib.sticky_header.sticky.StickyRecyclerHeadersDecorat
  * @version 1.0
  */
 public class BindingUtils {
+
+    /**
+     * 方便xml得到BindingConversion的辅助对象{@link Conversion<T>}
+     */
+    public static <T> Conversion<T> getConversion(T data) {
+        return new Conversion<>(data);
+    }
+
+    /**
+     * 为展示的区号数据增加“+”号
+     */
     public static String getShowCode(int countryCode) {
         return "+" + String.valueOf(countryCode);
     }
 
+    /**
+     * 组合中英文名展示数据
+     */
     public static String getShowName(String nameCn, String nameEn) {
         return nameCn + "(" + nameEn + ")";
-    }
-
-    public static <T> Conversion<T> getConversion(T data) {
-        return new Conversion<>(data);
     }
 
 
     /* ----------------------------- BindingAdapter ----------------------------- */
 
+    /**
+     * 数据绑定方式执行{@link ViewPager#setCurrentItem(int)}
+     */
     @BindingAdapter("currentItem")
     public static void setCurrentItem(ViewPager viewPager, int index) {
         viewPager.setCurrentItem(index, false);
     }
 
+    /**
+     * 数据绑定方式执行{@link ViewPager#setAdapter(PagerAdapter)}
+     */
     @BindingAdapter("adapter")
     public static void setAdapter(ViewPager viewPager, FragmentPagerAdapter pagerAdapter) {
         viewPager.setAdapter(pagerAdapter);
     }
 
+    /**
+     * 数据绑定方式执行{@link RecyclerView#setLayoutManager(RecyclerView.LayoutManager)}
+     */
     @BindingAdapter("layoutManager")
     public static void setLayoutManager(RecyclerView view, RecyclerView.LayoutManager manager) {
         view.setLayoutManager(manager);
     }
 
+    /**
+     * 数据绑定方式执行{@link RecyclerView#setAdapter(RecyclerView.Adapter)}
+     */
     @BindingAdapter("adapter")
     public static void setAdapter(RecyclerView view, RecyclerView.Adapter adapter) {
         view.setAdapter(adapter);
     }
 
+    /**
+     * 数据绑定方式执行{@link RecyclerView#addItemDecoration(RecyclerView.ItemDecoration)}
+     */
     @BindingAdapter("itemDecoration")
     public static void setItemDecoration(RecyclerView view, RecyclerView.ItemDecoration decoration) {
         view.addItemDecoration(decoration);
     }
 
+    /**
+     * 数据绑定方式执行{@link RecyclerView#addItemDecoration(RecyclerView.ItemDecoration)}
+     * （注：参数为{@link StickyRecyclerHeadersDecoration}）
+     */
     @BindingAdapter("stickyDecoration")
     public static void setStickyDecoration(RecyclerView view, StickyRecyclerHeadersDecoration decoration) {
         view.addItemDecoration(decoration);
@@ -67,11 +97,17 @@ public class BindingUtils {
 
     /* ----------------------------- BindingConversion ----------------------------- */
 
+    /**
+     * 使xml里的{@link Conversion<Integer>}数据转换成{@link ColorDrawable}
+     */
     @BindingConversion
     public static ColorDrawable convertToBgColor(Conversion<Integer> countryId) {
         return new ColorDrawable(Color.parseColor(countryId.getData() % 2 == 0 ? "#f4237a" : "#1296db"));
     }
 
+    /**
+     * 使xml里的{@link Conversion<Integer>}数据转换成{@link ColorInt}
+     */
     @BindingConversion
     @ColorInt
     public static int convertToTextColor(Conversion<Integer> countryId) {
